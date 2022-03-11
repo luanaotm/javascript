4735ea10d17bd51eb6d35013e052850cf436a0be
Luana Mendonça <luanaotm@gmail.com>
2022-03-11 17:16:21 -0300

    truthy e falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,32 @@
+// boolean
+const usuarioLogado = true
+const contapaga = false
+
+//truthy or falsy
+
+// 0 => false
+// 1 => true
+
+/*console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+*/
+
+// null ==> vazio ou nada
+// undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(typeof minhaVar) //undefined
+console.log(typeof varNull)  //object (erro antigo do JS)
+
+/*
+let numero = 3;
+let texto = "Alura"
+
+
+console.log(typeof numero) //number
+console.log(typeof texto)  //string
+*/
+

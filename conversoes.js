07762d0f63cea6e99c76acd383fba40f2551c8cf
Luana Mendonça <luanaotm@gmail.com>
2022-03-11 23:55:18 -0300

    conversões

---
+++ conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+//conversão implícita
+const numero = 456;
+const numeroString = Number("456");
+
+/*console.log(numero === numeroString) //false
+console.log(numero == numeroString) //true
+*/
+
+console.log(numero + Number(numeroString)) 
+
+//Conversão explícita
+
+//Number()
+//String()
+
+
